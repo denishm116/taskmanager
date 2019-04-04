0736eb5c3b65c31cd3775de1e3d7fb2389b61c81
denishm116 <48059988+denishm116@users.noreply.github.com>
2019-04-04 15:54:29 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+$email = ($_POST['email']);
+$password = ($_POST['password']);
+
+$pass = md5($password);
+
+//Подключаемся к БД
+$pdo = new PDO('mysql:host=localhost;dbname=taskmgr', 'root', '');
+
+//Выборка из БД для провелки логина
+$sql = "SELECT * FROM users";
+$statement = $pdo->prepare($sql);
+$array = $statement->execute();
+$array = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+//var_dump($array);
+//Проверяем данные (сравниваем с таблицей емаил и пароль)
+foreach ($array as $arr) {
+    if ($arr['email'] == $email) {
+        if ($arr['password'] == $pass) {
+            header('Refresh: 1; url=list.php');
+            echo "Добро пожаловать " . $arr['username'];
+            //Записываем переменную е-маил в сессию, чтобы использовать ее на странице списка задач.
+            $_SESSION['email'] = $email;
+
+            exit;
+        }
+    }
+}
+
+
+
+header('Location: login-form.php');
+//var_dump($pass);
